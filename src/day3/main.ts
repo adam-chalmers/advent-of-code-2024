70802b28324b188c62d6f7c7eb341b507707d251
adam-chalmers <48976102+adam-chalmers@users.noreply.github.com>
2024-12-03 23:38:53 +1000

    Why am I like this

--- src/day3/main.ts
+++ src/day3/main.ts
@@ -25,24 +25,32 @@ export class Day3 extends Day {
   }
 
   protected override part2(input: string): string {
-    const matches = input.matchAll(/do(?:n't)?\(\)?/g);
-    const commands: { enabled: boolean; index: number }[] = [];
-    for (const match of matches) {
-      commands.push({ enabled: match[0] === "do()", index: match.index });
-    }
+    // const matches = input.matchAll(/do(?:n't)?\(\)?/g);
+    // const commands: { enabled: boolean; index: number }[] = [];
+    // for (const match of matches) {
+    //   commands.push({ enabled: match[0] === "do()", index: match.index });
+    // }
 
-    const multiples = this.getMultiples(input);
-    let sum = 0;
-    let j = multiples.length - 1;
-    for (let i = commands.length - 1; i >= -1; i--) {
-      while (j >= 0 && (i === -1 || multiples[j].index > commands[i].index)) {
-        if (i === -1 || commands[i].enabled) {
-          sum += multiples[j].value;
-        }
-        j--;
-      }
-    }
+    // const multiples = this.getMultiples(input);
+    // let sum = 0;
+    // let j = multiples.length - 1;
+    // const matching: any[] = [];
+    // for (let i = commands.length - 1; i >= -1; i--) {
+    //   while (j >= 0 && (i === -1 || multiples[j].index > commands[i].index)) {
+    //     if (i === -1 || commands[i].enabled) {
+    //       sum += multiples[j].value;
+    //       matching.push(multiples[j]);
+    //     }
+    //     j--;
+    //   }
+    // }
 
-    return sum.toString();
+    // return sum.toString();
+    return this.part2_cursed(input);
+  }
+
+  // I can't believe I've done this
+  private part2_cursed(input: string): string {
+    return [...input.matchAll(/(?<=(?:^|do\(\))(?:(?!don't\(\)).)*?)mul\((\d{1,3}),(\d{1,3})\)/gs)].reduce((acc, curr) => acc + Number.parseInt(curr[1]) * Number.parseInt(curr[2]), 0).toString();
   }
 }
