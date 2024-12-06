f22934e838ec39d9ec96653109d4ae55a42be3cd
adam-chalmers <48976102+adam-chalmers@users.noreply.github.com>
2024-12-06 20:16:00 +1000

    Update day 4 with new Grid

--- src/day4/main.ts
+++ src/day4/main.ts
@@ -1,26 +1,16 @@
 import { Day } from "../common/day";
+import { Grid } from "../common/grid";
 
 export class Day4 extends Day {
   protected getDir(): string {
     return __dirname;
   }
 
-  private parse(input: string) {
-    const grid: string[][] = [];
-    const lines = input.split("\n");
-    for (const line of lines) {
-      const gridLine = line.split("");
-      grid.push(gridLine);
-    }
-
-    return grid;
-  }
-
-  private findCoords(grid: string[][], char: string) {
+  private findCoords(grid: Grid, char: string) {
     const coords: [x: number, y: number][] = [];
-    for (let y = 0; y < grid.length; y++) {
-      for (let x = 0; x < grid[y].length; x++) {
-        if (grid[y][x] == char) {
+    for (let y = 0; y < grid.height; y++) {
+      for (let x = 0; x < grid.width; x++) {
+        if (grid.get(x, y) == char) {
           coords.push([x, y]);
         }
       }
@@ -30,7 +20,7 @@ export class Day4 extends Day {
   }
 
   protected override part1(input: string): string {
-    const grid = this.parse(input);
+    const grid = Grid.fromInput({ input });
     const xCoords = this.findCoords(grid, "X");
 
     let count = 0;
@@ -38,9 +28,9 @@ export class Day4 extends Day {
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
           if (
-            grid[y + i]?.[x + j] === "M" &&
-            grid[y + i * 2]?.[x + j * 2] === "A" &&
-            grid[y + i * 3]?.[x + j * 3] === "S"
+            grid.tryGet(x + j, y + i) === "M" &&
+            grid.tryGet(x + j * 2, y + i * 2) === "A" &&
+            grid.tryGet(x + j * 3, y + i * 3) === "S"
           ) {
             count++;
           }
@@ -52,17 +42,21 @@ export class Day4 extends Day {
   }
 
   protected override part2(input: string): string {
-    const grid = this.parse(input);
+    const grid = Grid.fromInput({ input });
     const aCoords = this.findCoords(grid, "A");
 
     let count = 0;
     for (const [x, y] of aCoords) {
       const isForwards =
-        (grid[y - 1]?.[x - 1] === "M" && grid[y + 1]?.[x + 1] === "S") ||
-        (grid[y + 1]?.[x + 1] === "M" && grid[y - 1]?.[x - 1] === "S");
+        (grid.tryGet(x - 1, y - 1) === "M" &&
+          grid.tryGet(x + 1, y + 1) === "S") ||
+        (grid.tryGet(x + 1, y + 1) === "M" &&
+          grid.tryGet(x - 1, y - 1) === "S");
       const isBackwards =
-        (grid[y + 1]?.[x - 1] === "M" && grid[y - 1]?.[x + 1] === "S") ||
-        (grid[y - 1]?.[x + 1] === "M" && grid[y + 1]?.[x - 1] === "S");
+        (grid.tryGet(x - 1, y + 1) === "M" &&
+          grid.tryGet(x + 1, y - 1) === "S") ||
+        (grid.tryGet(x + 1, y - 1) === "M" &&
+          grid.tryGet(x - 1, y + 1) === "S");
       if (isForwards && isBackwards) {
         count++;
       }
